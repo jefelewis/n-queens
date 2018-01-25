d4d2a6f9a9c6e2f990a34aa08dc6638540a7ebd7
Hack Reactor Student <student@hackreactor.com>
2018-01-24 17:25:49 -0800

    first commit

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,37 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+     
+      var conflict = false;
+      var count = 0;
+            
+      // Iterate through the rows array (Mega Array)
+      for (var i = 0; i < this.rows()[rowIndex].length; i++) {
+        // Add up the array values
+        count = count + this.rows()[rowIndex][i];
+      }
+      // If the array values exist more than once, return false
+      if (count > 1) {
+        conflict = true;
+      } 
+      
+      return conflict;
+      
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // Check the index for the initial rowIndex
+      // Iterate through the first rowIndex
+      var conflict = false;
+      
+      for (var i = 0; i < this.rows().length; i++) {
+        if (this.hasRowConflictAt(i)) {
+          conflict = true;
+        } 
+      }
+      return conflict;
+      
     },
 
 
@@ -94,12 +119,35 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      
+      var conflict = false; // fixme
+      var count = 0;
+      
+      // Iterate through the rows array (Mega Array)
+      for (var i = 0; i < this.rows().length; i++) {
+        // If the array values exist more than once, return false
+        count += this.rows()[i][colIndex];
+      }
+      
+      if (count > 1) {
+        conflict = true;
+      }
+      
+      return conflict;         
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      
+      var conflict = false;
+      
+      // Iterate through the rows array (Mega Array)
+      for (var i = 0; i < this.rows().length; i++) {
+        if (this.hasColConflictAt(i)) {
+          conflict = true;
+        } 
+      }
+      return conflict;
     },
 
 
@@ -109,12 +157,170 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // console.log('major ', majorDiagonalColumnIndexAtFirstRow);
+      var conflict = false;
+      var count = 0;
+      
+      
+      // Across = 2
+      var across = majorDiagonalColumnIndexAtFirstRow;
+      
+      // Iterate through the Rows (Mega Array) (Going Right)
+      for (var i = 0; i < this.rows().length; i++) {
+        // If there is nothing to the right of it
+        if (this.rows()[i][across] !== undefined) {
+          // 
+          count += this.rows()[i][across];
+          across++;
+        }
+      }
+      
+      if (count > 1) {
+        conflict = true;
+      }
+      
+      return conflict;
+      
+      
+      
+      //---------------------------
+      // var conflict = false;
+      // var count = 0;
+      // var arrayIndex = majorDiagonalColumnIndexAtFirstRow; // arrayIndex = 0
+      // var verticalIndex = 0;
+      
+      
+      // // Iterate through the Rows (Mega Array)
+      // for (var i = 0; i < this.rows().length; i++) {
+        
+      //   // // Search Bottom Left
+      //   // for (var j = 0; j < this.rows().length; j--) {
+          
+          
+      //   // }
+        
+      //   // If there is nothing to the right of it
+      //   if (this.rows()[i][arrayIndex] !== undefined) {
+          
+      //     // Icrease the Count by 0 or 1, depending if there is a value
+      //     count += this.rows()[verticalIndex][arrayIndex];
+      //     // Increase the Current Index by 1
+      //     arrayIndex += 1;
+      //   }
+        
+      //   // console.log(arrayIndex);
+        
+      // }
+      
+      
+      // // // Iterate through the Rows (Mega Array) (Going Left)
+      // // for (var j = 0; j < this.rows().length; j--) {
+      // //   // Vertjcal Index (Left)
+      // //   var leftacross = majorDiagonalColumnIndexAtFirstRow;
+        
+      // //   // If there is notthing to the left of it
+      // //   if (this.rows()[j][leftacross]) {
+      // //     count += this.rows()[j][leftacross];
+      // //     leftacross += 1;
+          
+      // //   }
+      // // }
+      
+      
+      // if (count > 1) {
+      //   conflict = true;
+      // }
+      
+      // return conflict;
+      //---------------------------
+      
+      
+      
+      
+      
+      // // Column Index
+      // var searchIndex = 0;
+      // var firstIndex = 0;
+      
+      // // Find the index value of the first array
+      // // Iterate through the first row
+      // for (var i = 0; i < this.rows().length; i++) {
+        
+      //   // Iterate through the rowIndex array
+      //   for (var j = 0; j < this.rowIndex().length; j++) {
+        
+      //     // Find the Index value within the first array
+      //     if (this.rowIndex()[j] === 1) {
+      //       firstIndex = this.rowIndex()[j];
+      //     }
+          
+      //     // Add 1 to the current index count
+      //     searchIndex = firstIndex + 1;
+      //   }
+        
+        
+      //   // Search through the rest of the arrays
+      //   if (this.rowIndex()[searchIndex] === 1) {
+      //     conflict = true; 
+      //   }
+        
+      //   // Add 1 to the current index count
+      //   searchIndex += 1;  
+      // }
+      // return conflict;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var conflict = false; // fixme
+      // var outerIndex = 0;
+      // var innerIndex = 0;
+      
+      // Iterate through the Mega Array
+      
+      for (var i = 0; i < this.rows()[0].length; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          conflict = true;
+        }
+      }
+      
+ 
+      
+      
+      for (var i = 0; i < this.rows().length; i++) {
+        
+        // Iterate through the Inner array
+        for (var j = 0; j < this.rows()[i].length; j++) {
+          // Check if Array values pass the "hasMajorDiagonalConflictAt" test
+          if (this.hasMajorDiagonalConflictAt([i]) === true) {
+            conflict = true;
+          }
+        }
+        
+        // Add 1 to the Outer Index Array value
+        // outerIndex += 1;
+        
+      }      
+      return conflict;
+      
+      
+      
+      // // Searching the first array
+      // for (var i = 0; i < this.rows()[0].length; i++) {
+      //   if (this.hasMajorDiagonalConflictAt(i)) {
+      //     conflict = true;
+      //   }
+        
+      // }
+      
+      // // Searching the arrays under that 
+      // for (var j = 1; j < this.rows().length; j++) {
+      //   if (this.hasMajorDiagonalConflictAt(0)) {
+      //     conflict = true;
+          
+      //   }
+      // }
+      // return conflict;
     },
 
 
--- src/solvers.js
+++ src/solvers.js
@@ -12,11 +12,88 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme
+  
+  // Index
+  var index = 0;
+  
+  var innerArray = []; //--> ex. n=3 /---> [0,0,0]
+  // Build Default Inner Array
+  for (var k = 0; k < n; k++) {
+    innerArray.push(0);
+  }
+  
+  
+  
+  
+  // Iterate through for n times (n = amount of times to push)
+  for (var i = 0; i < n; i++) {
+    innerArray[index] = 1;
+    solution.push(innerArray);
+    index ++;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // // Construct Board
+  // for (var k = 0; k < n; k++) {
+  //   solution.push([]);
+  // }
+  
+  // return solution;
+  
+  // //---------
+  // //var Board = big array
+  // //rows Board[0], Board[1],....Board[n]
+  // //column all same i's 
+  // // n = 4
+
+  // // There can only be one "1" per array. At the index of that array, other arrays must = "0"
+  // //Row: there can only be 1 per array
+  // //Column: there can only be 1 per specified index (for all i=0)
+
+  // // Total Solutions
+  
+  
+  // var totalSolutions = 0;
+
+  // // Search Board[n] for Mega Array
+  // for (var i = 0; i < n; i++) {
+  //   // Iterate through the first array
+  //   for (var j = 0; j < solution[i].length; j++) {
+  //     var total = 0;
+
+  //     // Find the value within the first sub array
+  //     if (solution[i][j] === 1) {
+  //       total++;
+  //     }
+  //   }
+    
+  //   // If the value only exists once, return true
+  //   if (total <= 1) {
+  //     return true;
+  //     totalSolutions ++;
+      
+  //   } else {
+  //     return false;
+      
+  //   }
+
+  // }
+
+
+  // Search if the value exists at the same index for the rest of the arrays 
+
+
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
